e6d98198724919caf883805e4250bd0e3041a46b
Jeyhun Abilov <abilovv599@gmail.com>
2023-10-01 20:27:33 +0400

    minor change

--- Simple http server/index.js
+++ Simple http server/index.js
@@ -21,10 +21,10 @@ const customPortArg = process.argv.find((arg) => arg.startsWith('--port='));
 if (customPortArg) {
   const customPort = customPortArg.split('=')[1];
   server.listen(customPort, () => {
-    console.log(`Server is running on port ${customPort}`);
+    console.log(`Server is running on http://localhost:${customPort}/`);
   });
 } else {
   server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on http://localhost:3000/');
   });
 }
